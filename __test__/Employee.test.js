310d42497aca5ba70b04c5f57f1a2c217c4c86f0
Daniel <dkbaffour777@gmail.com>
2021-05-20 01:48:10 -0700

    test Employee object

---
+++ __test__/Employee.test.js
@@ -0,0 +1,17 @@
+const Employee = require('../lib/Employee');
+
+test('Creates an employee object', () => {
+    const name = "Kofi";
+    const id = 1;
+    const email = "kofi323@gmail.com";
+    const employee1 = new Employee(name, id, email);
+
+    expect(employee1.getName()).toBe("Kofi");
+    expect(employee1.getName()).toEqual(expect.any(String));
+
+    expect(employee1.getId()).toBe(1);
+    expect(employee1.getId()).toEqual(expect.any(Number));
+
+    expect(employee1.getEmail()).toBe("kofi323@gmail.com");
+    expect(employee1.getEmail()).toEqual(expect.any(String));
+});
